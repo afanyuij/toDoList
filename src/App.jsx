049f363496f678e7870181f5a4abfe68ae1d6982
afanyuij <afanyuij@gmail.com>
2025-03-31 00:43:33 +0200

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { ToDoList } from "./components/ToDoList";
-// import { NewToDoList } from "./components/NewToDoList"
 export default function App(){
   return(
     <div className="w-screen h-screen p-16 bg-gray-300">
